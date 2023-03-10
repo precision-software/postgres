41f667877077b61fc045d3c1935fc6f96956fd44
John Morris <john.morris@crunchydata.com>
2023-03-10 11:06:53 -0800

    Added kitchen sink test

--- src/test/storage/kitchenSinkTest.c
+++ src/test/storage/kitchenSinkTest.c
@@ -10,7 +10,16 @@
 
 static IoStack *createStack(size_t blockSize)
 {
-	return bufferedNew(1, vfdStackNew());
+	return 	bufferedNew(1024,
+				lz4CompressNew(1024,
+					bufferedNew(1024,
+						aeadNew("AES-256-GCM", blockSize, (Byte *) "0123456789ABCDEF0123456789ABCDEF", 32,
+							vfdStackNew())),
+					 bufferedNew(1024,
+						aeadNew("AES-256-GCM", blockSize, (Byte *) "0123456789ABCDEF0123456789ABCDEF", 32,
+							vfdStackNew()))));
+
+
 }
 
 void testMain()
@@ -18,6 +27,6 @@ void testMain()
 	system("rm -rf " TEST_DIR "raw; mkdir -p " TEST_DIR "raw");
 
 	beginTestGroup("Raw Files");
-	singleSeekTest(createStack, TEST_DIR "buffered/testfile_%u_%u.dat", 0, 3096);
-	seekTest(createStack, TEST_DIR "buffered/testfile_%u_%u.dat");
+	singleReadSeekTest(createStack, TEST_DIR "buffered/testfile_%u_%u.dat", 1027, 1024);
+	readSeekTest(createStack, TEST_DIR "buffered/testfile_%u_%u.dat");
 }
