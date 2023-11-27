47fa83fdd9d0f92f2ed32b96dd9909c581f5404d
John Morris <john.morris@crunchydata.com>
2023-11-27 11:44:48 -0800

    default to buffered

--- src/backend/storage/iostack/iostack.c
+++ src/backend/storage/iostack/iostack.c
@@ -48,8 +48,6 @@ selectIoStack(const char *path, int oflags, mode_t mode)
 	if (!ioStacksInitialized)
 		ioStackSetup();
 
-	return ioStackRaw;
-
 	/* TODO: create prototypes at beginning. Here, we just select them */
 	/* Look at oflags to determine which stack to use */
 	switch (oflags & PG_STACK_MASK)
@@ -58,7 +56,7 @@ selectIoStack(const char *path, int oflags, mode_t mode)
 		case PG_ENCRYPT:          return ioStackEncrypt;
 		case PG_ENCRYPT_PERM:     return ioStackEncryptPerm;
 		case PG_TESTSTACK:        return ioStackTest;
-		case 0:                   file_debug("Raw mode: path=%s oflags=0x%x", path, oflags); return ioStackRaw;
+		case 0:                   file_debug("Raw mode: path=%s oflags=0x%x", path, oflags); return ioStackPlain;
 
 		default: elog(FATAL, "Unrecognized I/O Stack oflag 0x%x", (oflags & PG_STACK_MASK));
 	}
