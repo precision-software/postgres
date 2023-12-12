1837a948f0e9567911f921b547a187c7d17f554e
John Morris <john.morris@crunchydata.com>
2023-12-12 14:11:29 -0800

    Pulled temporary files out of fd.c and created tempfile.c

--- src/backend/storage/file/fd.c
+++ src/backend/storage/file/fd.c
@@ -200,6 +200,7 @@ typedef struct vfd
 	int			fd;				/* current FD, or VFD_CLOSED if none */
 	unsigned short fdstate;		/* bitflags for VFD's state */
 	ResourceOwner resowner;		/* owner, for automatic cleanup */
+	SubTransactionId create_subid;  /* Subtransaction for automatic cleanup */
 	File		nextFree;		/* link to next free VFD, if in freelist */
 	File		lruMoreRecently;	/* doubly linked recency-of-use list */
 	File		lruLessRecently;
@@ -209,7 +210,7 @@ typedef struct vfd
 	int			fileFlags;		/* open(2) flags for (re)opening the file */
 	mode_t		fileMode;		/* mode to pass to open(2) */
 	FState      fState;         /* State to support FRead/FWrite/encyption */
-	SubTransactionId create_subid;  /* Subtransaction which owns this file */
+
 } Vfd;
 
 /*
@@ -241,7 +242,7 @@ static uint64 temporary_files_size = 0;
 
 /* Temporary file access initialized and not yet shut down? */
 #ifdef USE_ASSERT_CHECKING
-static bool temporary_files_allowed = false;
+PGDLLEXPORT bool temporary_files_allowed = false;
 #endif
 
 /*
@@ -276,23 +277,6 @@ static AllocateDesc *allocatedDescs = NULL;
  * Number of open "external" FDs reported to Reserve/ReleaseExternalFD.
  */
 static int	numExternalFDs = 0;
-
-/*
- * Number of temporary files opened during the current session;
- * this is used in generation of tempfile names.
- */
-static long tempFileCounter = 0;
-
-/*
- * Array of OIDs of temp tablespaces.  (Some entries may be InvalidOid,
- * indicating that the current database's default tablespace should be used.)
- * When numTempTableSpaces is -1, this has not been set in the current
- * transaction.
- */
-static Oid *tempTableSpaces = NULL;
-static int	numTempTableSpaces = -1;
-static int	nextTempTableSpace = 0;
-
 /*
  * Forward references
  */
@@ -341,7 +325,6 @@ static File AllocateVfd(void);
 static void FreeVfd(File file);
 
 static int	FileAccess(File file);
-static File OpenTemporaryFileInTablespace(Oid tblspcOid, bool rejectError);
 static bool reserveAllocatedDesc(void);
 static int	FreeDesc(AllocateDesc *desc);
 
@@ -1697,224 +1680,6 @@ PathNameDeleteTemporaryDir(const char *dirname)
 }
 
 /*
- * Open a temporary file that will disappear when we close it.
- *
- * This routine takes care of generating an appropriate tempfile name.
- * There's no need to pass in fileFlags or fileMode either, since only
- * one setting makes any sense for a temp file.
- *
- * Unless interXact is true, the file is remembered by CurrentResourceOwner
- * to ensure it's closed and deleted when it's no longer needed, typically at
- * the end-of-transaction. In most cases, you don't want temporary files to
- * outlive the transaction that created them, so this should be false -- but
- * if you need "somewhat" temporary storage, this might be useful. In either
- * case, the file is removed when the File is explicitly closed.
- */
-File
-OpenTemporaryFile(bool interXact)
-{
-	File		file = 0;
-
-	Assert(temporary_files_allowed);	/* check temp file access is up */
-
-	/*
-	 * Make sure the current resource owner has space for this File before we
-	 * open it, if we'll be registering it below.
-	 */
-	if (!interXact)
-		ResourceOwnerEnlarge(CurrentResourceOwner);
-
-	/*
-	 * If some temp tablespace(s) have been given to us, try to use the next
-	 * one.  If a given tablespace can't be found, we silently fall back to
-	 * the database's default tablespace.
-	 *
-	 * BUT: if the temp file is slated to outlive the current transaction,
-	 * force it into the database's default tablespace, so that it will not
-	 * pose a threat to possible tablespace drop attempts.
-	 */
-	if (numTempTableSpaces > 0 && !interXact)
-	{
-		Oid			tblspcOid = GetNextTempTableSpace();
-
-		if (OidIsValid(tblspcOid))
-			file = OpenTemporaryFileInTablespace(tblspcOid, false);
-	}
-
-	/*
-	 * If not, or if tablespace is bad, create in database's default
-	 * tablespace.  MyDatabaseTableSpace should normally be set before we get
-	 * here, but just in case it isn't, fall back to pg_default tablespace.
-	 */
-	if (file <= 0)
-		file = OpenTemporaryFileInTablespace(MyDatabaseTableSpace ?
-											 MyDatabaseTableSpace :
-											 DEFAULTTABLESPACE_OID,
-											 true);
-
-	/* Mark it for deletion at close and temporary file size limit */
-	VfdCache[file].fdstate |= FD_DELETE_AT_CLOSE | FD_TEMP_FILE_LIMIT;
-
-	/* Register it with the current resource owner */
-	if (!interXact)
-		RegisterTemporaryFile(file);
-
-	return file;
-}
-
-/*
- * Return the path of the temp directory in a given tablespace.
- */
-void
-TempTablespacePath(char *path, Oid tablespace)
-{
-	/*
-	 * Identify the tempfile directory for this tablespace.
-	 *
-	 * If someone tries to specify pg_global, use pg_default instead.
-	 */
-	if (tablespace == InvalidOid ||
-		tablespace == DEFAULTTABLESPACE_OID ||
-		tablespace == GLOBALTABLESPACE_OID)
-		snprintf(path, MAXPGPATH, "base/%s", PG_TEMP_FILES_DIR);
-	else
-	{
-		/* All other tablespaces are accessed via symlinks */
-		snprintf(path, MAXPGPATH, "pg_tblspc/%u/%s/%s",
-				 tablespace, TABLESPACE_VERSION_DIRECTORY,
-				 PG_TEMP_FILES_DIR);
-	}
-}
-
-/*
- * Open a temporary file in a specific tablespace.
- * Subroutine for OpenTemporaryFile, which see for details.
- */
-static File
-OpenTemporaryFileInTablespace(Oid tblspcOid, bool rejectError)
-{
-	char		tempdirpath[MAXPGPATH];
-	char		tempfilepath[MAXPGPATH];
-	File		file;
-
-	TempTablespacePath(tempdirpath, tblspcOid);
-
-	/*
-	 * Generate a tempfile name that should be unique within the current
-	 * database instance.
-	 */
-	snprintf(tempfilepath, sizeof(tempfilepath), "%s/%s%d.%ld",
-			 tempdirpath, PG_TEMP_FILE_PREFIX, MyProcPid, tempFileCounter++);
-
-	/*
-	 * Open the file.  Note: we don't use O_EXCL, in case there is an orphaned
-	 * temp file that can be reused.
-	 */
-	file = PathNameOpenFile(tempfilepath,
-							O_RDWR | O_CREAT | O_TRUNC | PG_BINARY);
-	if (file <= 0)
-	{
-		/*
-		 * We might need to create the tablespace's tempfile directory, if no
-		 * one has yet done so.
-		 *
-		 * Don't check for an error from MakePGDirectory; it could fail if
-		 * someone else just did the same thing.  If it doesn't work then
-		 * we'll bomb out on the second create attempt, instead.
-		 */
-		(void) MakePGDirectory(tempdirpath);
-
-		file = PathNameOpenFile(tempfilepath,
-								O_RDWR | O_CREAT | O_TRUNC | PG_BINARY);
-		if (file <= 0 && rejectError)
-			elog(ERROR, "could not create temporary file \"%s\": %m",
-				 tempfilepath);
-	}
-
-	return file;
-}
-
-
-/*
- * Create a new file.  The directory containing it must already exist.  Files
- * created this way are subject to temp_file_limit and are automatically
- * closed at end of transaction, but are not automatically deleted on close
- * because they are intended to be shared between cooperating backends.
- *
- * If the file is inside the top-level temporary directory, its name should
- * begin with PG_TEMP_FILE_PREFIX so that it can be identified as temporary
- * and deleted at startup by RemovePgTempFiles().  Alternatively, it can be
- * inside a directory created with PathNameCreateTemporaryDir(), in which case
- * the prefix isn't needed.
- */
-File
-PathNameCreateTemporaryFile(const char *path, bool error_on_failure)
-{
-	File		file;
-
-	Assert(temporary_files_allowed);	/* check temp file access is up */
-
-	ResourceOwnerEnlarge(CurrentResourceOwner);
-
-	/*
-	 * Open the file.  Note: we don't use O_EXCL, in case there is an orphaned
-	 * temp file that can be reused.
-	 */
-	file = PathNameOpenFile(path, O_RDWR | O_CREAT | O_TRUNC | PG_BINARY);
-	if (file <= 0)
-	{
-		if (error_on_failure)
-			ereport(ERROR,
-					(errcode_for_file_access(),
-					 errmsg("could not create temporary file \"%s\": %m",
-							path)));
-		else
-			return file;
-	}
-
-	/* Mark it for temp_file_limit accounting. */
-	VfdCache[file].fdstate |= FD_TEMP_FILE_LIMIT;
-
-	/* Register it for automatic close. */
-	RegisterTemporaryFile(file);
-
-	return file;
-}
-
-/*
- * Open a file that was created with PathNameCreateTemporaryFile, possibly in
- * another backend.  Files opened this way don't count against the
- * temp_file_limit of the caller, are automatically closed at the end of the
- * transaction but are not deleted on close.
- */
-File
-PathNameOpenTemporaryFile(const char *path, int mode)
-{
-	File		file;
-
-	Assert(temporary_files_allowed);	/* check temp file access is up */
-
-	ResourceOwnerEnlarge(CurrentResourceOwner);
-
-	file = PathNameOpenFile(path, mode | PG_BINARY);
-
-	/* If no such file, then we don't raise an error. */
-	if (file <= 0 && errno != ENOENT)
-		ereport(ERROR,
-				(errcode_for_file_access(),
-				 errmsg("could not open temporary file \"%s\": %m",
-						path)));
-
-	if (file > 0)
-	{
-		/* Register it for automatic close. */
-		RegisterTemporaryFile(file);
-	}
-
-	return file;
-}
-
-/*
  * Delete a file by pathname.  Return true if the file existed, false if
  * didn't.
  */
@@ -3027,94 +2792,6 @@ closeAllVfds(void)
 
 
 /*
- * SetTempTablespaces
- *
- * Define a list (actually an array) of OIDs of tablespaces to use for
- * temporary files.  This list will be used until end of transaction,
- * unless this function is called again before then.  It is caller's
- * responsibility that the passed-in array has adequate lifespan (typically
- * it'd be allocated in TopTransactionContext).
- *
- * Some entries of the array may be InvalidOid, indicating that the current
- * database's default tablespace should be used.
- */
-void
-SetTempTablespaces(Oid *tableSpaces, int numSpaces)
-{
-	Assert(numSpaces >= 0);
-	tempTableSpaces = tableSpaces;
-	numTempTableSpaces = numSpaces;
-
-	/*
-	 * Select a random starting point in the list.  This is to minimize
-	 * conflicts between backends that are most likely sharing the same list
-	 * of temp tablespaces.  Note that if we create multiple temp files in the
-	 * same transaction, we'll advance circularly through the list --- this
-	 * ensures that large temporary sort files are nicely spread across all
-	 * available tablespaces.
-	 */
-	if (numSpaces > 1)
-		nextTempTableSpace = pg_prng_uint64_range(&pg_global_prng_state,
-												  0, numSpaces - 1);
-	else
-		nextTempTableSpace = 0;
-}
-
-/*
- * TempTablespacesAreSet
- *
- * Returns true if SetTempTablespaces has been called in current transaction.
- * (This is just so that tablespaces.c doesn't need its own per-transaction
- * state.)
- */
-bool
-TempTablespacesAreSet(void)
-{
-	return (numTempTableSpaces >= 0);
-}
-
-/*
- * GetTempTablespaces
- *
- * Populate an array with the OIDs of the tablespaces that should be used for
- * temporary files.  (Some entries may be InvalidOid, indicating that the
- * current database's default tablespace should be used.)  At most numSpaces
- * entries will be filled.
- * Returns the number of OIDs that were copied into the output array.
- */
-int
-GetTempTablespaces(Oid *tableSpaces, int numSpaces)
-{
-	int			i;
-
-	Assert(TempTablespacesAreSet());
-	for (i = 0; i < numTempTableSpaces && i < numSpaces; ++i)
-		tableSpaces[i] = tempTableSpaces[i];
-
-	return i;
-}
-
-/*
- * GetNextTempTableSpace
- *
- * Select the next temp tablespace to use.  A result of InvalidOid means
- * to use the current database's default tablespace.
- */
-Oid
-GetNextTempTableSpace(void)
-{
-	if (numTempTableSpaces > 0)
-	{
-		/* Advance nextTempTableSpace counter with wraparound */
-		if (++nextTempTableSpace >= numTempTableSpaces)
-			nextTempTableSpace = 0;
-		return tempTableSpaces[nextTempTableSpace];
-	}
-	return InvalidOid;
-}
-
-
-/*
  * AtEOSubXact_Files
  *
  * Take care of subtransaction commit/abort.  At abort, we close temp files
@@ -3166,7 +2843,15 @@ AtEOSubXact_Files(bool isCommit, SubTransactionId mySubid,
  *
  * The isCommit flag is used only to decide whether to emit warnings about
  * unclosed files.
+ *
+ * For historical reasons, we reset the tempfile tablespaces here.
+ * Eventually this should move to tempfile.c, but for now, we import
+ * the values and reset them here. Note we should move the temp
+ * directory routines to tempfile.c as well.
  */
+extern PGDLLIMPORT Oid *tempTableSpaces;
+extern PGDLLIMPORT int numTempTableSpaces;
+
 void
 AtEOXact_Files(bool isCommit)
 {
@@ -3216,7 +2901,12 @@ BeforeShmemExit_Files(int code, Datum arg)
  *
  * Note we call FClose to close the file. FClose will invoke FileClose
  * for files which do not use I/O stacks.
+ *
+ * Tempfiles have been moved to tempfile.c, but we wtill need to initialize
+ * tempfiles. As a workaround, we import the tempfile variables and reset them
+ * here.
  */
+
 static void
 CleanupTempFiles(bool isCommit, bool isProcExit)
 {
